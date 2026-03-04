66167acc634ed9a97e5b37cf3026ab0ea2ced784
archboo <katashi1994@yandex.ru>
2026-03-05 01:01:44 +0300

    fix

--- quasar.config.ts
+++ quasar.config.ts
@@ -50,7 +50,7 @@ export default defineConfig((/* ctx */) => {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      publicPath: '/buildhouse',
+      publicPath: '/buildhouse/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
