14273f9550163c6e7476cf8ab8c3ff3c9367ad6f
archboo <katashi1994@yandex.ru>
2026-03-05 01:18:50 +0300

    fix src

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,18 +1,28 @@
-import type { RouteRecordRaw } from 'vue-router';
+import { RouteRecordRaw } from 'vue-router'
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [
+      { path: '', component: () => import('pages/HomePage.vue') },
+      {
+        path: '/doma',
+        component: () => import('pages/DomaMainPage.vue'),
+        children: [
+          {
+            path: 'doma-iz-brusa',
+            name: 'doma-iz-brusa',
+            component: () => import('src/pages/DomaPage/DomaIzBrusaPage.vue'),
+          },
+        ],
+      },
+    ],
   },
-
-  // Always leave this as last one,
-  // but you can also remove it
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue'),
   },
-];
+]
 
-export default routes;
+export default routes
