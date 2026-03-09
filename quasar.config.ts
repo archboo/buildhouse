b01122dd9dbf24a36f416011aa4b1c6070ef3a2f
archboo <katashi1994@yandex.ru>
2026-03-09 14:21:14 +0300

    fix error

--- quasar.config.ts
+++ quasar.config.ts
@@ -47,14 +47,14 @@ export default defineConfig((/* ctx */) => {
         // extendTsConfig (tsConfig) {}
       },
 
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
       // vueRouterBase,
       // vueDevtools,
       // vueOptionsAPI: false,
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      publicPath: '/buildhouse/',
+      publicPath: process.env.PUBLIC_PATH || '/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
